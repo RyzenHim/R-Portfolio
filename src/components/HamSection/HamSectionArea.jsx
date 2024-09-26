33de69b9051b0c401e5edff4861d8a7753bb81e9
Himanshu upadhyay <himanshuupadhyay210>
2024-09-26 21:21:22 +0530

    animation added in nav

--- src/components/HamSection/HamSectionArea.jsx
+++
@@ -1,5 +0,0 @@
-// import "./HamSectionArea.css";
-// const HamSectionArea = (prop) => {
-//   return <div className={`${isActive ? prop.active : prop.notactive}`}></div>;
-// };
-// export default HamSectionArea;
